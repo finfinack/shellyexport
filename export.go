6d68d0904609f090bdc4eb270b82409ce3b99e8b
Martin Suess <martin.suess@kun.ch>
2024-12-26 08:17:14 +0100

    move stats to separate func

--- export.go
+++ export.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -23,17 +24,10 @@ const (
 	urlPath = "/v2/statistics/power-consumption/em-3p"
 )
 
-func main() {
-	flag.Parse()
-
-	cfg, err := config.ReadFromFile(*configFile)
-	if err != nil {
-		log.Fatalf("unable to read config: %s\n", err)
-	}
-
+func pullStatistics(cfg *config.Config) (*shelly.PowerConsumptionStatistics, error) {
 	url, err := url.Parse(cfg.Server)
 	if err != nil {
-		log.Fatalf("invalid server specified in config (%q): %s\n", cfg.Server, err)
+		return nil, fmt.Errorf("invalid server specified in config (%q): %s\n", cfg.Server, err)
 	}
 	url.Path = "/v2/statistics/power-consumption/em-3p"
 	q := url.Query()
@@ -47,7 +41,7 @@ func main() {
 
 	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
 	if err != nil {
-		log.Fatalf("unable to create request: %s\n", err)
+		return nil, fmt.Errorf("unable to create request: %s\n", err)
 	}
 	if cfg.UserAgent != "" {
 		req.Header.Add("User-Agent", cfg.UserAgent)
@@ -55,27 +49,42 @@ func main() {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatalf("unable to fetch data: %s\n", err)
+		return nil, fmt.Errorf("unable to fetch data: %s\n", err)
 	}
 	if http.StatusOK != resp.StatusCode {
 		b, _ := io.ReadAll(resp.Body)
-		log.Fatalf("unable to fetch data (response code %d): %s", resp.StatusCode, b)
+		return nil, fmt.Errorf("unable to fetch data (response code %d): %s", resp.StatusCode, b)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("unable to read response body: %s\n", err)
+		return nil, fmt.Errorf("unable to read response body: %s\n", err)
 	}
 
 	stats := &shelly.PowerConsumptionStatistics{}
 	if err := json.Unmarshal(body, stats); err != nil {
-		log.Fatalf("unable to parse body as JSON: %s", err)
+		return nil, fmt.Errorf("unable to parse body as JSON: %s", err)
 	}
 
 	if stats.Interval != "day" {
-		log.Fatalf("returned interval is not supported: %q\n", stats.Interval)
+		return nil, fmt.Errorf("returned interval is not supported: %q\n", stats.Interval)
 	}
 
-	stats = shelly.NormalizePowerConsumptionStatistics(stats, time.Time(cfg.Timeframe.From), time.Time(cfg.Timeframe.To))
+	return shelly.NormalizePowerConsumptionStatistics(stats, time.Time(cfg.Timeframe.From), time.Time(cfg.Timeframe.To)), nil
+}
+
+func main() {
+	flag.Parse()
+
+	cfg, err := config.ReadFromFile(*configFile)
+	if err != nil {
+		log.Fatalf("unable to read config: %s\n", err)
+	}
+
+	stats, err := pullStatistics(cfg)
+	if err != nil {
+		log.Fatalf("unable to pull statistics: %s", err)
+	}
+
 	export.ToCSV(stats, os.Stdout)
 }
